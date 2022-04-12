3675ee9027891d18b95ea02173bdb780936625c1
Oriol Tomas <oriol.tomas@benchmarkcapital.co.uk>
2022-04-12 16:00:14 +0200

    [ORI-000] Add access to blob storage

--- api/Function1.cs
+++ api/Function1.cs
@@ -1,6 +1,9 @@
+using System;
 using System.IO;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -8,6 +11,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.VisualBasic;
 using Newtonsoft.Json;
 
 namespace api
@@ -22,14 +26,15 @@ namespace api
         }
 
         [FunctionName("Function1")]
-        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiOperation(operationId: "RunFunction1", tags: new[] { "name" })]
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-        public async Task<IActionResult> Run(
+        public async Task<IActionResult> RunFunction1(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-
+            
+            // Response
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
@@ -41,6 +46,38 @@ namespace api
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("Function2")]
+        [OpenApiOperation(operationId: "RunFunction2")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public async Task<IActionResult> RunFunction2(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            // Create blob client
+            var connectionStr = Environment.GetEnvironmentVariable("StorageAccountConnectionString");
+            const string containerName = "testcontainer";
+            var blobContainerClient = new BlobContainerClient(connectionStr, containerName);
+            await blobContainerClient.CreateIfNotExistsAsync();
+
+            // Get a reference to a blob in a container 
+            const string blobName = "1.json";
+            var blobClient = blobContainerClient.GetBlobClient(blobName);
+
+            // Upload the content
+            var testObj = new TestObject() { TestProperty = DateAndTime.Now.ToLongDateString() };
+            var byteArray = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(testObj));
+            await using var memoryStream = new MemoryStream(byteArray);
+            await blobClient.UploadAsync(memoryStream, overwrite: true);
+            
+            return new OkObjectResult("Completed");
+        }
+
+        public class TestObject
+        {
+            public string TestProperty { get; set; }
         }
     }
 }
